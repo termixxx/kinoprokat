30749adc52870ab71543a40240eed2ab8a0fb4c4
zagorovs <azagorovskiy@phoenixit.ru>
2026-06-06 01:06:21 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/kinoprokat/KinoprokatApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.kinoprokat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoprokatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoprokatApplication.class, args);
+    }
+
+}
